3092ebc8fb0923188cb67663013c22327a176236
mansonboy <mrdaurelthomas@gmail.com>
2022-12-02 16:47:54 +0100

    add migrate creditInfos

---
+++ my-project/database/migrations/2022_12_02_151800_create_credit_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credit_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('placeOfBirth', 50);
+            $table->string('nationality', 50);
+            $table->float('budgets', 10, 2);
+            $table->string('contract', 3);
+            $table->date('contractDate');
+            $table->string('banquet',25);
+            $table->string('professionnalStatus', 50);
+            $table->string('familySituation',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credit_infos');
+    }
+};
