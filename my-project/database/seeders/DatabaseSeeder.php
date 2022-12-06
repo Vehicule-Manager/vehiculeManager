bbdd3b86a4687c7678ef7552f4e2acdcfbdda9ba
Fournier Julien <julien@MacBook-Pro-de-Fournier.local>
2022-12-06 13:55:57 +0100

    add factory/seeder brand gearboxe status energie

--- my-project/database/seeders/DatabaseSeeder.php
+++ my-project/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,14 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Seeder;
 use App\Models\Type;
 use App\Models\CreditInfo;
-
+use App\Models\Brand;
+use App\Models\Energie;
+use App\Models\GearBoxe;
+use App\Models\Status;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,7 +22,10 @@ class DatabaseSeeder extends Seeder
     {
         CreditInfo::factory(5)->create();
         Type::factory(5)->create();
-        // \App\Models\User::factory(10)->create();
+        Brand::factory(5)->create();
+        GearBoxe::factory(5)->create();
+        Status::factory(5)->create();
+        Energie::factory(5)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
